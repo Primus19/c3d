2ac6728833153b2655f6ebd1c06f120c92d4a388
ahandresf <36319841ahandresf@users.noreply.github.com>
2020-04-28 21:11:24 -0500

    code working

--- extract_features_test.py
+++ extract_features_test.py
@@ -2,14 +2,16 @@ import skvideo.io
 from c3d import C3D
 from keras.models import Model
 from sports1M_utils import preprocess_input, decode_predictions
+VIDEO_PATH='cat.mp4'
 
 base_model = C3D(weights='sports1M')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc6').output)
 
-vid_path = 'homerun.mp4'
+vid_path = VIDEO_PATH
 vid = skvideo.io.vread(vid_path)
 # Select 16 frames from video
 vid = vid[40:56]
 x = preprocess_input(vid)
 
 features = model.predict(x)
+print(features)
