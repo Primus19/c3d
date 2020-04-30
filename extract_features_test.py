b2801566bb25a84806fec0b9ef3520c7aace2acb
Primus19 <51384509+Primus19@users.noreply.github.com>
2020-04-29 19:38:12 -0500

    Update extract_features_test.py
    video path

--- extract_features_test.py
+++ extract_features_test.py
@@ -7,7 +7,7 @@ VIDEO_PATH='cat.mp4'
 base_model = C3D(weights='sports1M')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc6').output)
 
-vid_path = VIDEO_PATH
+vid_path = C:\Users\primu\Downloads\Anomaly-Videos-Part-1.zip\Anomaly-Videos-Part-1\Arrest
 vid = skvideo.io.vread(vid_path)
 # Select 16 frames from video
 vid = vid[40:56]
